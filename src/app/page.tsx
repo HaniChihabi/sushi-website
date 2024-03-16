403d7222571ea5346a0c2c3d81f035cf1e80942f
HaniChihabi <hanichihabih@gmail.com>
2024-03-16 06:09:43 +0100

    new section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,9 +18,18 @@ export default function Home() {
         </div>
         </div>
       </section>
-      <section className="h-screen w-full flex justify-center bg-orange-50 ">
-        <div className="w-1/2 relative top-20 gap-10">
-        <h1 className="flex justify-center items-center text-6xl text-center font-display mb-10">TOKYO-INSPIRED JAPANESE CUISINE & COCKTAILS</h1>
+      <section className="h-screen w-full flex justify-center text-white">
+      <div className="w-full h-screen absolute flex justify-center items-center  -z-10">
+          <Image
+            src={"/marmordark.jpg"}
+            alt="Coffee Shop"
+            objectFit="cover"
+            layout="fill"
+            priority
+          />
+        </div>
+        <div className="w-1/2 relative top-10 gap-10">
+        <h1 className="flex justify-center items-center text-6xl font-display py-20 text-white">TOKYO-INSPIRED JAPANESE CUISINE & COCKTAILS</h1>
         <div className="w-full h-1/2 flex">
           <div className="w-1/3 h-full relative">
             <Image
@@ -31,13 +40,45 @@ export default function Home() {
                   priority
                 />
             </div>
-            <div className="w-1/2 h-full ml-20 text-l leading-relaxed mt-1">
+            <div className="w-1/2 h-full ml-20 text-l leading-relaxed mt-10">
               <p>I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. I’m a great place for you to tell a story and let your users know a little more about you.</p>
-              <button type="button"  className="transition duration-300 ease-in-out hover:bg-purple-300 font-light border py-3 px-7 bg-orange-100 rounded-full text-xl mt-28">Button</button>
-
+              <button type="button"  className="transition duration-300 ease-in-out hover:bg-purple-300 font-light py-3 px-7 bg-orange-500 rounded-full text-xl text-black mt-28">Explore the Menu</button>
+              <div className="w-full h-1/2 relative top-40 ml-32">
+            <Image
+                  src={"/mainsushi.jpg"}
+                  alt="Coffee Shop"
+                  objectFit="cover"
+                  layout="fill"
+                  priority
+                />
             </div>
+          </div>
         </div>
-</div>
+      </div>
+      </section>
+      <section className="h-screen w-full flex justify-center">
+      <div className="w-full h-screen flex justify-center items-center absolute -z-10">
+          <Image
+            src={"/marmorwhite.jpg"}
+            alt="Coffee Shop"
+            objectFit="cover"
+            layout="fill"
+            priority
+          />        
+        </div>
+        <div className=" absolute left-40 mt-40 text-6xl text-black">
+          <h1>DROP BY,</h1>
+          <h1>IT'S SUSHI TIME SOMEWHERE</h1>
+          <div className="w-1/2 h-[30ch] flex justify-center items-center absolute -z-10">
+          <Image
+            src={"/themainburger.jpg"}
+            alt="Coffee Shop"
+            objectFit="cover"
+            layout="fill"
+            priority
+          />        
+        </div>
+        </div>
       </section>
     </main>
   );

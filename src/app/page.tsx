ae16e6055b6c7d1620e36caad161117006f64800
HaniChihabi <hanichihabih@gmail.com>
2024-03-18 15:11:53 +0100

    cleaned code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,14 +14,65 @@ export default function Home() {
             layout="fill"
             priority
           />
-        <div>
-          <h1 className=" font-bold text-black text-8xl z-10 relative bottom-32 ">Sushi Store</h1>
-          <h1 className=" font-bold text-orange-500 text-6xl z-10 relative bottom-20 left-5">Crafted with Passion, Served with Joy</h1>
-        </div>
+          <div>
+            <h1 className=" font-bold text-black text-7xl z-10 relative bottom-32 ">Sushi Store</h1>
+            <h1 className=" font-bold text-orange-500 text-6xl z-10 relative bottom-20 left-5">Crafted with Passion, Served with Joy</h1>
+          </div>
         </div>
       </section>
       <section className="h-screen w-full flex justify-center">
-      <div className="w-full h-screen absolute flex justify-center items-center  -z-10">
+        <div className="w-full h-screen absolute flex justify-center items-center  -z-10">
+          <Image
+            src={"/marmordark.jpg"}
+            alt="Coffee Shop"
+            objectFit="cover"
+            layout="fill"
+            priority
+          />
+        </div>
+
+        <div className="w-1/2 relative top-10 gap-10 text-white">
+          <h1 className="flex justify-center items-center text-5xl font-bold py-20">TOKYO-INSPIRED JAPANESE CUISINE & COCKTAILS</h1>
+          <div className="w-full h-1/2 flex">
+            <div className="w-1/3 h-full relative">
+              <Image
+                src={"/mainsushi.jpg"}
+                alt="Coffee Shop"
+                objectFit="cover"
+                layout="fill"
+                priority
+              />
+            </div>
+            <div className="w-1/2 h-full ml-20 text-l leading-relaxed mt-10">
+              <p>I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. I’m a great place for you to tell a story and let your users know a little more about you.</p>
+              <button type="button"  className="transition duration-300 ease-in-out hover:bg-purple-300 font-light py-3 px-7 bg-orange-500 rounded-full text-xl text-black mt-28">Explore the Menu</button>
+            </div>
+          </div>
+          <div className="w-full h-1/2 relative top-0 flex">
+            <div className="w-full h-1/2 relative">
+              <Image
+                src={"/mainsushi.jpg"}
+                alt="Coffee Shop"
+                objectFit="cover"
+                layout="fill"
+                priority
+              />
+            </div>
+            <div className="w-full h-1/2 relative">
+              <Image
+                src={"/mainsushi.jpg"}
+                alt="Coffee Shop"
+                objectFit="cover"
+                layout="fill"
+                priority
+              />
+            </div>
+          </div>
+      </div>
+      </section>
+      
+      <section className="h-screen w-full flex justify-center text-white">
+        <div className="w-full h-screen absolute flex justify-center items-center  -z-10">
           <Image
             src={"/marmorwhite.jpg"}
             alt="Coffee Shop"
@@ -29,54 +80,30 @@ export default function Home() {
             layout="fill"
             priority
           />
-        </div>
-        <div className="w-1/2 relative top-10 gap-10 text-black">
-        <h1 className="flex justify-center items-center text-5xl font-bold py-20">TOKYO-INSPIRED JAPANESE CUISINE & COCKTAILS</h1>
-        <div className="w-full h-1/2 flex">
-          <div className="w-1/3 h-full relative">
-            <Image
-                  src={"/mainsushi.jpg"}
-                  alt="Coffee Shop"
-                  objectFit="cover"
-                  layout="fill"
-                  priority
-                />
-            </div>
-          
-            <div className="w-1/2 h-full ml-20 text-l leading-relaxed mt-10">
-              <p>I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. I’m a great place for you to tell a story and let your users know a little more about you.</p>
-              <button type="button"  className="transition duration-300 ease-in-out hover:bg-purple-300 font-light py-3 px-7 bg-orange-500 rounded-full text-xl text-black mt-28">Explore the Menu</button>
-              <div>
-                
-              </div>
-              
+        </div>        
+        <div className=" absolute left-40 mt-40 text-black gap-x-10">
+          <div className="text-5xl text-orange-500 font-bold">
+            <h1>DROP BY,</h1>
+            <h1>IT'S SUSHI TIME SOMEWHERE</h1>
           </div>
-          
+          <div className="w-1/4 mt-20 h-f text-l text-black">
+            <p className="text-xl">I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font.</p>
+            <button type="button"  className="transition duration-300 ease-in-out hover:bg-purple-300 font-light py-3 px-7 bg-orange-500 rounded-full text-xl text-black mt-28">Explore the Menu</button>
+          </div> 
+          <div className="w-2/5 h-full flex justify-center items-center relative ml-40 mt-20"> 
+            <Image
+              src={"/themainburger.jpg"}
+              alt="Coffee Shop"
+              objectFit="cover"
+              layout="fill"
+              priority
+            />   
+          </div>
         </div>
-        <div className="w-full h-1/2 relative top-0 flex">
-                  <div className="w-full h-1/2 relative">
-                    <Image
-                      src={"/mainsushi.jpg"}
-                      alt="Coffee Shop"
-                      objectFit="cover"
-                      layout="fill"
-                      priority
-                    />
-                </div>
-                <div className="w-full h-1/2 relative">
-                    <Image
-                      src={"/mainsushi.jpg"}
-                      alt="Coffee Shop"
-                      objectFit="cover"
-                      layout="fill"
-                      priority
-                    />
-                </div>
-              </div>
-      </div>
       </section>
+
       <section className="h-[150vh] w-full flex justify-center">
-      <div className="w-full h-[150vh] flex justify-center items-center absolute -z-10">
+        <div className="w-full h-[150vh] flex justify-center items-center absolute -z-10">
           <Image
             src={"/marmordark.jpg"}
             alt="Coffee Shop"
@@ -87,10 +114,10 @@ export default function Home() {
         </div>
         <div className=" absolute left-40 mt-40 text-black gap-x-10">
           <div className="text-5xl text-orange-500 font-bold">
-          <h1>DROP BY,</h1>
-          <h1>IT'S SUSHI TIME SOMEWHERE</h1>
+            <h1>DROP BY,</h1>
+            <h1>IT'S SUSHI TIME SOMEWHERE</h1>
           </div>
-            <div className="flex flex-row w-full h-[100vh] gap-x-64">
+          <div className="flex flex-row w-full h-[100vh] gap-x-64">
             <div className="w-2/5 flex justify-center items-center relative mt-20"> {/* Adjusted for image */}
               <Image
                 src={"/themainburger.jpg"}
@@ -99,44 +126,28 @@ export default function Home() {
                 layout="fill"
                 priority
               />   
-              </div>    
-              <div className="w-1/4 mt-20 h-f text-l text-white">
-                <div>
-                  <p className="text-xl">I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font.</p>
-                </div>
-                <div className="mt-40">                
+            </div>    
+            <div className="w-1/4 mt-20 h-f text-l text-white">
+              <p className="text-xl">I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font.</p>
+              <div className="mt-40">                
                 <CustomDivider thickness="h-0.5" color="bg-white" margin="my-8" />
-                  <h1 className="text-3xl font-bold">Address</h1>
-                  <div className="text-l mt-2">
-                    <p>500 TERRY FRANCINE STREET</p>
-                    <p>SAN FRANCISCO, CA 94158</p>
-                  </div>
+                <h1 className="text-3xl font-bold">Address</h1>
+                <div className="text-l mt-2">
+                  <p>500 TERRY FRANCINE STREET</p>
+                  <p>SAN FRANCISCO, CA 94158</p>
                 </div>
-                <div className="border-5 w-20 h-1"></div>
-                <div className="mt-20">                
+              </div>
+              <div className="mt-20">                
                 <CustomDivider thickness="h-0.5" color="bg-white" margin="my-8" />
-                  <h1 className="text-3xl font-bold">Address</h1>
-                  <div className="text-l mt-2">
-                    <p>500 TERRY FRANCINE STREET</p>
-                    <p>SAN FRANCISCO, CA 94158</p>
-                  </div>
+                <h1 className="text-3xl font-bold">Address</h1>
+                <div className="text-l mt-2">
+                  <p>500 TERRY FRANCINE STREET</p>
+                  <p>SAN FRANCISCO, CA 94158</p>
                 </div>
-              </div> 
-
-            
+              </div>
+            </div>
           </div>
         </div>
-      </section>
-      <section className="h-screen w-full flex justify-center text-white">
-      <div className="w-full h-screen absolute flex justify-center items-center  -z-10">
-          <Image
-            src={"/marmordark.jpg"}
-            alt="Coffee Shop"
-            objectFit="cover"
-            layout="fill"
-            priority
-          />
-        </div>        
       </section>
     </main>
   );

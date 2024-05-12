38c9dc471ca6886b49ae9ffb2b7a422280afc593
HaniChihabi <hanichihabih@gmail.com>
2024-05-12 10:36:26 +0200

    new try

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,12 +1,10 @@
-"use client"
+"use client";
 import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import { MenuOutlined } from '@ant-design/icons';
-export default function Navbar() {
-    // State for showing or hiding the navbar based on scroll position
-    const [show, setShow] = useState(true);
 
+export default function Navbar() {
     // State for tracking the last scroll position
     const [lastScrollY, setLastScrollY] = useState(0);
     
@@ -18,12 +16,11 @@ export default function Navbar() {
 
     // Function to control the visibility and background color of the navbar on scroll
     const controlNavbar = () => {
-        // Hide navbar and change background to black when scrolling down and past 100px
+        // Hide navbar and change background to black when scrolling down and past 5px
         if (window.scrollY > lastScrollY && window.scrollY > 5) {
             setNavbarBg('bg-black'); // Set navbar background to black
         } else {
             // Show navbar and change background to transparent when scrolling up or at the top of the page
-            setShow(true);
             setNavbarBg(window.scrollY > 100 ? 'bg-black' : 'transparent'); // Keep navbar background black if not at the top
         }
         // Update last scroll position state
@@ -50,18 +47,18 @@ export default function Navbar() {
     }, [showMenu]);
 
     return (
-<nav className={`fixed flex z-50 justify-between w-full p-7 transition-opacity duration-500 ${show ? 'opacity-100' : 'opacity-0'} ${navbarBg} navbar-transition`}>
-            <Link href="/#">
-                    <div className='w-12 h-12 relative bottom-1 ml-6 mt-'>
-                        <Image
-                            src="/newestsushilogo.png"
-                            alt="Sushi Store"
-                            width={60}
-                            height={60}
-                            objectFit="contain"
-                            priority
-                        />
-                    </div>
+        <nav className={`fixed flex z-50 justify-between w-full p-7 transition-opacity duration-500 ${navbarBg} navbar-transition`}>
+            <Link href="/">
+                <div className='w-12 h-12 relative bottom-1 ml-6 mt-'>
+                    <Image
+                        src="/newestsushilogo.png"
+                        alt="Sushi Store"
+                        width={60}
+                        height={60}
+                        objectFit="contain"
+                        priority
+                    />
+                </div>
             </Link>
             <div className='hidden lg:block'>
                 <ul className="flex gap-x-10 mr-20 text-l font-light">
@@ -69,10 +66,9 @@ export default function Navbar() {
                     <li className="transition duration-300 ease-in-out hover:bg-orange-600 font-light  py-3 px-5 bg-orange-50 rounded-full text-xl"><Link href="/about">CALL US 1234 12345678</Link></li>
                     <li className="transition duration-300 ease-in-out hover:bg-orange-500 font-light py-3 px-5 bg-orange-50 rounded-full text-xl"><Link href="/about">ORDER ONLINE</Link></li>
                     <li className="hover:text-amber-50 hover:pointer transition-colors text-orange-500 mt-0 text-4xl">
-                    <MenuOutlined onClick={() => setShowMenu(!showMenu)} />
+                        <MenuOutlined onClick={() => setShowMenu(!showMenu)} />
                     </li>
-                    
-                    </ul>
+                </ul>
             </div>
             <div className='lg:hidden'>
                 {/* Toggle button for mobile menu */}
@@ -88,6 +84,7 @@ export default function Navbar() {
                         <li className="hover:text-gray-600 hover:pointer transition-colors"><Link href="/contact">Contact</Link></li>
                     </ul>
                 </div>
-            )}        </nav>
+            )}
+        </nav>
     );
 };
--- src/app/components/Rating.tsx
+++ src/app/components/Rating.tsx
@@ -10,28 +10,28 @@ export default function Rating() {
           <div className="mt-0 w-full grid grid-cols-2 grid-rows-2 py-24 px-24 relative bottom-10">
               <div className=" p-6 border-t-2 border-l-2 bg-gray-50 border-black">
                   <div className="px-8 py-12">
-                      <p className="text-l w-1/2">“Im a testimonial. Click to edit me and add text that says something nice about you and your services.”</p>
+                      <p className="text-l w-1/2">Im a testimonial. Click to edit me and add text that says something nice about you and your services.</p>
                       <h1 className="mt-5 font-bold">hgv ghbh</h1>
                       <FiveStar />
                   </div>
               </div>
               <div className="bg-gray-50 p-6 border-t-2 border-r-2 border-l-2 border-black">
                   <div className="px-8 py-12">
-                      <p className="text-l w-1/2">“Im a testimonial. Click to edit me and add text that says something nice about you and your services.”</p>
+                      <p className="text-l w-1/2">Im a testimonial. Click to edit me and add text that says something nice about you and your services.</p>
                       <h1 className="mt-5 font-bold">hgv ghbh</h1>
                       <FiveStar />
                   </div>
               </div>
               <div className="bg-gray-50 p-6 border-t-2 border-l-2 border-b-2 border-black">
                   <div className="px-8 py-12">
-                      <p className="text-l w-1/2">“Im a testimonial. Click to edit me and add text that says something nice about you and your services.”</p>
+                      <p className="text-l w-1/2">Im a testimonial. Click to edit me and add text that says something nice about you and your services.</p>
                       <h1 className="mt-5 font-bold">hgv ghbh</h1>
                       <FiveStar />
                   </div>
               </div>
               <div className="bg-gray-50 p-6 border-2 border-black">
                   <div className="px-8 py-12">
-                      <p className="text-l w-1/2">“Im a testimonial. Click to edit me and add text that says something nice about you and your services.”</p>
+                      <p className="text-l w-1/2">Im a testimonial. Click to edit me and add text that says something nice about you and your services.</p>
                       <h1 className="mt-5 font-bold">hgv ghbh</h1>
                       <FiveStar />
                   </div>
